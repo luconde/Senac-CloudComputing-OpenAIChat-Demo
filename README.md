9fa774fe2b9ec25ac3c59c19b694690ff591afae
LucondeAtOracleNotebook <luconde@outlook.com>
2023-09-22 00:50:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Visão Geral
+Este projeto Console baseado em Donet Core com C# que realiza perguntas para o OpenAI através de sua API.
+
+Projeto construído para a disciplina **Cloud Computing e Internet das Coisas** do curso **Tecnologia em Sistemas para Internet (TSI) do Senac-SP**.
+
+# Autor
+**Luciano Condé de Souza (luconde@gmail.com)**  
+**Data da criação do projeto**: 2023-09-21  
+**Data da última atualização**: 2023-09-21  
+**Versão**: 1.0.0
+
+## Disclaimer
+O seguinte material foi construído a partir de referências publicadas na Internet, livros e artigos acadêmicos. As referências foram utilizadas de sites e posts na Internet, não há qualquer propósito de plagiar os autores, em caso de pedidos de adição do autor, pode encontrar em contato pelo email luconde@gmail.com. A simplificação de certos conteúdos tem o único propósito didático para facilitar o entendimento dos mesmos para os alunos.
+
+# Notas da versão 
+## Versão 1.0.0
+1. Disponibilidade da versão
+
+# Detalhes técnicos
+
+## Funcionalidades
+1. Recebe a pergunta do usuário e envia para o OpenAI
+
+## Pré-requisitos
+1. Subscription Ativa do modo de desenvolvimento no OpenAI
+
+# Informações adicionais
+Utilize o arquivo appsettings.json.template para criar o próprio appsettings.json. Adicione a chave do OpenAI.
---
+++ src/OpenAIChatConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using OpenAI_API.Completions;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+// Capturando as configurações do arquivo de configurações
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+IConfiguration config = builder.Build();
+string strOpenAIAppKey = config["OpenAI:AppKey"];
+
+// Cria o objeto da API para OpenAI
+var api = new OpenAI_API.OpenAIAPI(strOpenAIAppKey);
+
+// Prepara as variaveis de pergunta e resposta
+string strQuestion = string.Empty;
+string strAnswer = string.Empty;
+
+// Parametros de Versão
+Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileVersionInfo.ProductName);
+Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileVersionInfo.LegalCopyright);
+Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileVersionInfo.ProductVersion);
+
+// Enquanto o usuario não pressionar CTRL + C mantenha o loop de pedido de pergunta
+while (true)
+{
+    Console.WriteLine("Aperte CTRL + C para fechar o aplicativo.");
+    Console.WriteLine("-----------------------------------------");
+    Console.WriteLine("Digite a sua questão:");
+    strQuestion = Console.ReadLine();
+
+    // Envia para o OpenAI o pedido de resposta com máximo de 1,024 tokens
+    var results = api.Completions.CreateCompletionsAsync(
+        new CompletionRequest(
+            strQuestion, temperature: 0.1, max_tokens: 1024,
+            model: OpenAI_API.Models.Model.DavinciText
+            )
+        , 1).Result;
+
+    // Exibe as respostas
+    strAnswer = results.Completions[0].Text;
+    Console.WriteLine(strAnswer);
+    Console.WriteLine();
+    Console.WriteLine("-----------------------------------------");
+}
